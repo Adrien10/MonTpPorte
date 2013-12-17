6e40f31813f34c22131d43449065d4a69a9e7d26
Adrien10 <adrienbenj@free.fr>
2013-12-17 16:06:01 +0100

    Model modification structure

--- src/com/imie/montpporte/model/Commande.java
+++ src/com/imie/montpporte/model/Commande.java
@@ -5,9 +5,53 @@ package com.imie.montpporte.model;
 public class Commande {
 
 	private int id;
+	protected int quantite;
+	protected int typeItem;
+	protected String materiaux;
+	protected String idClient;
 
 	public Commande() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public int getTypeItem() {
+		return typeItem;
+	}
+
+	public void setTypeItem(int typeItem) {
+		this.typeItem = typeItem;
+	}
+
+	public String getMateriaux() {
+		return materiaux;
+	}
+
+	public void setMateriaux(String materiaux) {
+		this.materiaux = materiaux;
+	}
+
+	public String getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(String idClient) {
+		this.idClient = idClient;
 	}
 
 }
--- src/com/imie/montpporte/model/Production.java
+++ src/com/imie/montpporte/model/Production.java
@@ -2,8 +2,36 @@ package com.imie.montpporte.model;
 
 public class Production {
 
+	private int id;
+	protected Commande commade;
+	protected String typeProduit;
+	
 	public Production() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Commande getCommade() {
+		return commade;
+	}
+
+	public void setCommade(Commande commade) {
+		this.commade = commade;
+	}
+
+	public String getTypeProduit() {
+		return typeProduit;
+	}
+
+	public void setTypeProduit(String typeProduit) {
+		this.typeProduit = typeProduit;
 	}
 
 }
