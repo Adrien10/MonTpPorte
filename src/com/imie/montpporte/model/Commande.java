f0fccce4b6105e0aa5ba2bd94447a6c9395c551d
erwanf <erwanf@gmail.com>
2013-12-17 15:07:58 +0100

    Modif Erwan

--- src/com/imie/montpporte/model/Commande.java
+++ src/com/imie/montpporte/model/Commande.java
@@ -5,7 +5,12 @@ package com.imie.montpporte.model;
 public class Commande {
 
 	private int idClient;
-
+	private int numeroCommande;
+	private String typeFabrication;
+	private int QuantiteFabrication;
+	
+	
+	
 	public Commande() {
 		// TODO Auto-generated constructor stub
 	}
--- src/com/imie/montpporte/model/Production.java
+++ src/com/imie/montpporte/model/Production.java
@@ -1,6 +1,10 @@
 package com.imie.montpporte.model;
 
+import com.imie.montpporte.model.Commande;
+
 public class Production {
+
+	private Commande commande;
 
 	public Production() {
 		// TODO Auto-generated constructor stub
