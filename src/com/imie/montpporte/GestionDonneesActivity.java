9e81b5ae4d1773b1c7ca23dafde11f6a5f903358
qint1 <qint1@hotmail.fr>
2013-12-19 09:08:42 +0100

    gestiondonne

--- src/com/imie/montpporte/GestionDonneesActivity.java
+++ src/com/imie/montpporte/GestionDonneesActivity.java
@@ -4,8 +4,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import java.util.Locale;
-
-
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.SearchManager;
@@ -46,8 +44,9 @@ public class GestionDonneesActivity extends Activity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // set a custom shadow that overlays the main content when the drawer opens
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set custom shadow that overlays the main content when  drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, 
+        		GravityCompat.START);
         // set up the drawer's list view with items and click listener
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_item, mGestionTitles));
@@ -72,27 +71,26 @@ public class GestionDonneesActivity extends Activity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    
-
-   
-
     /* The click listner for ListView in the navigation drawer */
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+    private class DrawerItemClickListener implements 
+    	ListView.OnItemClickListener {
         @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+        		long id) {
             selectItem(position);
         }
     }
 
     private void selectItem(int position) {
         // update the main content by replacing fragments
-        Fragment fragment = new PlanetFragment();
+        Fragment fragment = new actionFragment();
         Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+        args.putInt(actionFragment.ARG_PLANET_NUMBER, position);
         fragment.setArguments(args);
 
         FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        fragmentManager.beginTransaction().replace(R.id.content_frame,
+        		fragment).commit();
 
         // update selected item and title, then close the drawer
         mDrawerList.setItemChecked(position, true);
@@ -100,40 +98,48 @@ public class GestionDonneesActivity extends Activity {
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
+    /**
+     * Change title name
+     */
     @Override
     public void setTitle(CharSequence title) {
         mTitle = title;
         getActionBar().setTitle(mTitle);
     }
 
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
+ 
 
    
 
     /**
      * Fragment that appears in the "content_frame", shows a planet
      */
-    public static class PlanetFragment extends Fragment {
+    public static class actionFragment extends Fragment {
         public static final String ARG_PLANET_NUMBER = "planet_number";
 
-        public PlanetFragment() {
+        public actionFragment() {
             // Empty constructor required for fragment subclasses
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_action,
+            		container, false);
             int i = getArguments().getInt(ARG_PLANET_NUMBER);
-            String planet = getResources().getStringArray(R.array.action_array)[i];
-
-            int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
-                            "drawable", getActivity().getPackageName());
-            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
-            getActivity().setTitle(planet);
+            String action = getResources().getStringArray(R.array.action_array)[i];
+//
+//            int imageId = getResources().getIdentifier(action.toLowerCase(Locale.getDefault()),
+//                            "drawable", getActivity().getPackageName());
+//            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+            getActivity().setTitle(action);
+//            switch(i) {
+//            case 1:
+//               
+//                
+//            default:
+//                return super.onOptionsItemSelected(item);
+//            }
             return rootView;
         }
     }
