de46192dc72432596c6f45f0696cc0b9a80311f9
qint1 <qint1@hotmail.fr>
2013-12-19 09:16:37 +0100

    code non utilisé

--- src/com/imie/montpporte/GestionDonneesActivity.java
+++ src/com/imie/montpporte/GestionDonneesActivity.java
@@ -1,36 +1,24 @@
 package com.imie.montpporte;
 
-import android.app.ActionBar;
+
 import android.app.Activity;
 import android.os.Bundle;
-import java.util.Locale;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.SearchManager;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class GestionDonneesActivity extends Activity {
 	 	private DrawerLayout mDrawerLayout;
 	    private ListView mDrawerList;
-	    private ActionBarDrawerToggle mDrawerToggle;
 
-	    private CharSequence mDrawerTitle;
 	    private CharSequence mTitle;
 	    private String[] mGestionTitles;
 	    @Override
@@ -38,7 +26,7 @@ public class GestionDonneesActivity extends Activity {
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_gestion_donnees);
-	    mTitle = mDrawerTitle = getTitle();
+	    mTitle = getTitle();
 	    
 	    mGestionTitles = getResources().getStringArray(R.array.action_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
